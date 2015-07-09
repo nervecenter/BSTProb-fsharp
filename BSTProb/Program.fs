15cb5f90e15ec14ecac2681c2139fd7d4cd850df
Nerve <nervecenter@outlook.com>
2015-07-08 21:41:17 -0400

    Implement lookup dictionary.

--- BSTProb/Program.fs
+++ BSTProb/Program.fs
@@ -16,11 +16,6 @@ type BST = {
     right  : BST option;
 }
 
-//type BSTcost = {
-//    tree : BST;
-//    cost : double;
-//}
-
 let KeyCost key height =
     key.prob * ( (double)height + 1.0 )
 
@@ -41,38 +36,45 @@ let rec PrintTree tree =
     | None -> ()
 
 let OptimalTree (keys : List<Key>) =
-    //let lookup = new Dictionary<int*int*int, BST>()
+    let lookup = new Dictionary<int*int*int, BST>()
 
     let rec MinCostTree i j h =
-        match i with
-        | i when i = j -> let leaf = { ele = keys.[i]; 
-                                       height = h; 
-                                       cost = KeyCost keys.[i] h; 
-                                       left = None; 
-                                       right = None }
-                          //lookup.Add( (i, j, h), leaf )
-                          leaf
-        | _ -> let treeList = new List<BST>()
-               for k in i .. j do
-                   let leftChild =  match k with
-                                    | k when k = i -> None
-                                    | _ -> Some(MinCostTree i (k-1) (h+1))
-                   let rightChild = match k with
-                                    | k when k = j -> None
-                                    | _ -> Some(MinCostTree (k+1) j (h+1))
-                   let leftCost =   match leftChild with
-                                    | Some t -> t.cost
-                                    | None -> 0.0
-                   let rightCost =  match rightChild with
-                                    | Some t -> t.cost
-                                    | None -> 0.0
-                   treeList.Add( { ele = keys.[k]; 
-                                   height = h; 
-                                   left = leftChild; 
-                                   right = rightChild; 
-                                   cost = leftCost + rightCost + KeyCost keys.[k] h } )
-               List.ofSeq treeList
-               |> List.minBy (fun t -> t.cost)
+        match lookup.ContainsKey (i, j, h) with
+        | true ->  printfn "In dict: %d, %d, %d" i j h
+                   lookup.Item (i, j, h)
+        | false -> match i with
+                   | i when i = j -> let leaf = { ele = keys.[i]; 
+                                                  height = h; 
+                                                  cost = KeyCost keys.[i] h; 
+                                                  left = None; 
+                                                  right = None }
+                                     printfn "Not in dict: %d, %d, %d - adding" i j h
+                                     lookup.Add( (i, j, h), leaf )
+                                     leaf
+                   | _ -> let treeList = new List<BST>()
+                          for k in i .. j do
+                              let leftChild =  match k with
+                                               | k when k = i -> None
+                                               | _ -> Some( MinCostTree i (k-1) (h+1) )
+                              let rightChild = match k with
+                                               | k when k = j -> None
+                                               | _ -> Some( MinCostTree (k+1) j (h+1) )
+                              let leftCost =   match leftChild with
+                                               | Some t -> t.cost
+                                               | None -> 0.0
+                              let rightCost =  match rightChild with
+                                               | Some t -> t.cost
+                                               | None -> 0.0
+                              treeList.Add( { ele = keys.[k]; 
+                                              height = h; 
+                                              left = leftChild; 
+                                              right = rightChild; 
+                                              cost = leftCost + rightCost + KeyCost keys.[k] h } )
+                          let mintree = List.ofSeq treeList
+                                        |> List.minBy (fun t -> t.cost)
+                          printfn "Not in dict: %d, %d, %d - adding" i j h
+                          lookup.Add( (i, j, h), mintree )
+                          mintree
     MinCostTree 0 (keys.Count - 1) 0
 
 
@@ -80,13 +82,14 @@ let OptimalTree (keys : List<Key>) =
 let main argv = 
     let keys = new List<Key>()
     keys.Add({num = 1; prob = 0.25})
-    keys.Add({num = 2; prob = 0.05})
-    keys.Add({num = 3; prob = 0.1})
-    keys.Add({num = 4; prob = 0.07})
-    keys.Add({num = 5; prob = 0.22})
-    keys.Add({num = 6; prob = 0.09})
-    keys.Add({num = 7; prob = 0.13})
-    keys.Add({num = 8; prob = 0.09})
+    keys.Add({num = 2; prob = 0.2})
+    keys.Add({num = 3; prob = 0.05})
+    keys.Add({num = 4; prob = 0.2})
+    keys.Add({num = 5; prob = 0.3})
+//    keys.Add({num = 6; prob = 0.09})
+//    keys.Add({num = 7; prob = 0.13})
+//    keys.Add({num = 8; prob = 0.09})
     let otree = OptimalTree keys
+    printfn "\n"
     PrintTree otree
     0
